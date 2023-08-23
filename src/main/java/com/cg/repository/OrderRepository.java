dd38c2739f3ea15298f2e025aceb01dcc3d1ce55
longg2504 <longg2504@gmail.com>
2023-08-23 11:13:34 +0700

    upload

--- src/main/java/com/cg/repository/OrderRepository.java
+++ src/main/java/com/cg/repository/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.cg.repository;
 
-public interface OrderRepository {
+import com.cg.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
 }
