17aa64c2e9ee6d818a6a3cf01e2c53e7cb19948b
lthn99 <lenhung14101999@gmail.com>
2023-08-23 09:12:21 +0700

    future/user

--- src/main/java/com/cg/repository/RoleRepository.java
+++ src/main/java/com/cg/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.cg.repository;
 
-public interface RoleRepository {
+import com.cg.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
 }
