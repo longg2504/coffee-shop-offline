4eed37ab75e798423f480bd25b66bf88edb1ce40
longg2504 <longg2504@gmail.com>
2023-08-23 14:57:50 +0700

    upload feature/tableOrder

---
+++ src/main/java/com/cg/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.cg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "login/login";
+    }
+
+    @GetMapping("/shop")
+    public String showShopPage() {
+        return "shop/index";
+    }
+}
