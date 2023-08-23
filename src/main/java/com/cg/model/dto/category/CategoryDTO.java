6c94d03dec38e9f913ea09aff2493927ae3ca4fe
longg2504 <longg2504@gmail.com>
2023-08-23 15:29:01 +0700

    upload

--- src/main/java/com/cg/model/dto/category/CategoryDTO.java
+++ src/main/java/com/cg/model/dto/category/CategoryDTO.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+<<<<<<< HEAD
 import lombok.experimental.Accessors;
+=======
+>>>>>>> cd831af (upload)
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+<<<<<<< HEAD
 @Accessors(chain = true)
+=======
+>>>>>>> cd831af (upload)
 public class CategoryDTO {
 
     private Long id;
