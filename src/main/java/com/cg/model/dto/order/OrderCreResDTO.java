cfb861bac738c583b99bea0bd378e05515a2fbe9
longg2504 <longg2504@gmail.com>
2023-08-24 08:16:08 +0700

    upload

---
+++ src/main/java/com/cg/model/dto/order/OrderCreResDTO.java
@@ -0,0 +1,19 @@
+package com.cg.model.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+public class OrderCreResDTO {
+    private Long tableId;
+    private BigDecimal totalAmount;
+}
--- src/main/java/com/cg/service/order/IOrderService.java
+++ src/main/java/com/cg/service/order/IOrderService.java
@@ -2,7 +2,14 @@ package com.cg.service.order;
 
 
 import com.cg.model.Order;
+import com.cg.model.TableOrder;
 import com.cg.service.IGeneralService;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IOrderService extends IGeneralService<Order,Long> {
+    Optional<Order> findByTableId(Long tableId);
+
+    List<Order> findByTableOrderAndPaid(TableOrder tableOrder, Boolean paid);
 }
